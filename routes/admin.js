2901f781619b2ce8b024ba3effd30eb435a3ceff
Arvind Andrion <bonandrion@gmail.com>
2019-05-02 22:45:24 -0600

    setup google auth

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// ***************************
+// Admin
+// ***************************
+router.get('/', async function(req, res, next) {
+  res.render('admin', { 
+    title: 'Admin page'
+  });
 });
 
 module.exports = router;
