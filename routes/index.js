639fe3214c143c119bc9a2fcf0df4fff8e204eed
Arvind Andrion <bonandrion@gmail.com>
2019-05-04 22:58:38 -0600

    configuring authentication

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
+var url  = require('url');
 var router = express.Router();
+const { dump } = require('dumper.js');
+var _ = require('lodash');
+// var User = require('../models/user');
+var passport = require('passport');
+const LocalStrategy = require('passport-local');
+var { isAuth, googleCallback } = require('./utils');
+require('dotenv').config();
+
+// ******************************
+// Passport
+// ******************************
+passport.use(new LocalStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  passReqToCallback: true
+},
+function(req, accessToken, refreshToken, profile, done) {
+  googleCallback(req, accessToken, refreshToken, profile, done);
+}
+));
+passport.serializeUser(function(user, done) { done(null, user); });
+passport.deserializeUser(function(user, done) { done(null, user); });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// ******************************
+// Login & Logout
+// ******************************
+// Get
+router.get('/login', function(req, res, next) {
+  const loginErr = req.query && req.query.access ? 'Email / Password didn\'t match' : '';
+  res.render('login', { title: 'Welcome to Ending with Berto', loginErr: loginErr });
+});
+// Post
+router.post('/login', passport.authenticate(
+    'google', 
+    {
+      successRedirect: '/admin',
+      failureRedirect: '/login?access=false',
+    }
+  )
+);
+// Logout
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
---
+++ routes/utils.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var _ = require('lodash');
+const { dump } = require('dumper.js');
+// Database
+// var User = require('../models/user');
+// var Card = require('../models/card');
+
+module.exports = {
+  isAuth: async function(req, res, next) {
+    if (req.user) {
+      const user =  await User.findOne({ '_id' : req.user._id }, function(err, data) {``
+        if (err) throw err;
+        return data;
+      })
+      const isPremium = user.account && user.account === "premium" ? true : false;
+      const isLimitReached = user.slots && user.slots.length >= 2 ? true : false;
+      res.locals.user = user;
+      res.locals.isLoggedIn = true;
+      res.locals.isPremium = isPremium;
+      res.locals.isLimitReached = isLimitReached;
+    } else {
+      res.locals.isLoggedIn = false;
+    }
+    next();
+  },
+  googleCallback: function(req, accessToken, refreshToken, profile, done) {
+    const email = profile.emails[0].value === "bonandrion@gmail.com";
+    // if (err) {
+      
+    // } else {
+    //   return done(err);
+    // }
+
+  //   }
+  //   User.findOne({ 'username' : profile.emails[0].value }, function(err, user) {
+  //     if (err) {
+  //       return done(err);
+  //     } else if (user) {
+  //       return done(null, user);
+  //     } else {
+  //       var newUser = new User({
+  //         profile_id: profile.id,
+  //         name: profile.displayName,
+  //         username: profile.emails[0].value,
+  //         gender: profile.gender,
+  //         image: profile.photos[0].value,
+  //         account: 'free',
+  //         origin: 'google'
+  //       });
+  //       newUser.save(function(err) {
+  //         if (err) throw err;
+  //         return done(null, newUser);
+  //       });
+  //     }
+  //   });
+  }
+};
